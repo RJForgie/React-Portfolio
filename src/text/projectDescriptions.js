0444244543edac1d499a93a223997bb99f0ff37c
Ryan Forgie <ryan.j.forgie@gmail.com>
2018-02-06 21:56:34 +0000

    fix links

--- src/text/projectDescriptions.js
+++ src/text/projectDescriptions.js
@@ -1,8 +1,8 @@
 const projectDescriptions = 
 { 
-    "MoneyCashBoard":"MoneyCashBoard Text ferhfbelrfwberwbfruhbfkhefbj refnwelrfnrwenfhrwebfr ernflrnflrfnrnh rfwerlfjnlrejfnrljnewfljnr", 
-    "TravelSafe":"TravelSafe Text fhadshfnhjdsbfhjdsbfhjbsdhjfbdhfbhdjbfdh snbfhjdsbfhjsdf dhfnhdbfhjdbfhdjfbajsdf hfnhjdsfbhjsfbdhfbd hfbh", 
-    "WalkableEdinburgh":"Walkable Edinburgh Text sdjhfbdshjfb jdhsbfadjshfb dshjfbhjdsabfh  jdbfhdsbhjfbvhbfhjbhbg dhsbhjgbfdgh jfbghjfdbghjfbgh" 
+    "MoneyCashBoard":"Budgeting web app that allows the user to track and analyse spending. Built using Ruby, Sinatra, PostgreSQl and HTML/CSS.", 
+    "TravelSafe":"Travel Safety app that uses multiple public APIs to obtain dynamic data. Built using JavaScript, Node.js, HTML and CSS.", 
+    "WalkableEdinburgh":"Travel app that allows users to plan a route through pre-curated tourist attractions in Edinburgh. Built using JavaScript, Node.js, MongoDB, HTML and CSS." 
 };
 
     export default projectDescriptions;
